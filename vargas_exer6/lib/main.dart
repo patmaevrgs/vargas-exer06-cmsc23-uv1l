7a331c05c26d8bbb47fb6d773414719da18502e7
patmaevrgs <pivargas2@up.edu.ph>
2024-05-05 00:09:53 +0800

    feat: added necessary files

---
+++ vargas_exer6/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'screen/MyCart.dart';
+import 'screen/MyCatalog.dart';
+import 'package:provider/provider.dart';
+import 'provider/shoppingcart_provider.dart';
+import 'screen/Checkout.dart';
+
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => ShoppingCart()),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/cart": (context) => const MyCart(),
+        "/products": (context) => const MyCatalog(),
+        "/checkout": (context) => const Checkout(),
+      },
+      home: const MyCatalog(),
+    );
+  }
+}
